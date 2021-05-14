18b7dd0d4ff11aa8715a8e9655e83d40ab2d3354
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-05-14 20:54:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.include?(cheese_types)
+  array.include?(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
